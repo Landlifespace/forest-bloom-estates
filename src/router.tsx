65c0acf8e42a8470a90439d61124213c17c90bf9
Landlifespace <jinu@landlifespace.in>
2026-05-07 22:21:02 +0530

    COMPLETE DEPLOY

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
+    basepath: "/forest-bloom-estates",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
